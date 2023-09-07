ccfdcb41ccb279f7d3ae33ddf3c4a65fc80442c5
Patrick Hulce <patrick.hulce@compass.com>
2023-09-07 16:14:40 -0500

    fix: run autopep8

--- src/handwritten-ocr.py
+++ src/handwritten-ocr.py
@@ -6,17 +6,20 @@ from PIL import Image
 
 
 def main():
-  processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
-  model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
+    processor = TrOCRProcessor.from_pretrained(
+        "microsoft/trocr-base-handwritten")
+    model = VisionEncoderDecoderModel.from_pretrained(
+        "microsoft/trocr-base-handwritten")
 
-  image = Image.open(sys.argv[1]).convert("RGB")
+    image = Image.open(sys.argv[1]).convert("RGB")
 
-  pixel_values = processor(image, return_tensors="pt").pixel_values
-  generated_ids = model.generate(pixel_values)
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
 
-  generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
-  print(json.dumps(generated_text))
+    generated_text = processor.batch_decode(
+        generated_ids, skip_special_tokens=True)
+    print(json.dumps(generated_text))
+
 
 if __name__ == "__main__":
     main()
-
